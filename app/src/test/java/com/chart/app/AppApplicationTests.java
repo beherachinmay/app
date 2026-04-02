3ba500d4448f0ad9cd51e2fce6bdc8bb583bf390
beherachinmay <123244749+beherachinmay@users.noreply.github.com>
2026-04-02 23:17:58 +0530

    Initial Files

---
+++ app/src/test/java/com/chart/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chart.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
